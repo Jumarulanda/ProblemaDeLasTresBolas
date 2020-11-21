6dad58356a6049064f01cdaaf12c56c3d9e4a015
maryialejandra <maryi.carvajal@udea.edu.co>
2020-11-21 17:42:04 -0500

    comments

--- RK4_symplectic/RK4_symplectic.h
+++ RK4_symplectic/RK4_symplectic.h
@@ -7,14 +7,15 @@ using namespace std;
 class rk4_s
 {
  public:
-  rk4_s(vector  <double>,vector  <double>,vector <vector<double>> (*)(vector <double>,vector<double>));
+  rk4_s(vector  <double>,vector  <double>,vector <vector<double>> (*)(double,vector <double>,vector<double>));
   vector <vector <double>> rk4(const double, const int);
  private:
-  vector <vector<double>> (*f)(vector<double>,vector<double>);
+  vector <vector<double>> (*f)(double, vector<double>,vector<double>);
   vector <double> vector_state_q;
   vector <double> vector_state_p;
-  vector <double> next(const double);
-  void v_q(double,double);
-  void v_p(double,double);
+  vector <double> next(const double,double);
+  void v_q(double,double,double);
+  void v_p(double,double,double);
   void print(vector <double> const &);
+  double t;
 };
--- RK4_symplectic/read_rk4.cpp
+++ RK4_symplectic/read_rk4.cpp
@@ -5,25 +5,30 @@
 
 using namespace std;
 
-vector <vector<double>> f(vector <double>,vector <double>);
+vector <vector<double>> f(double,vector <double>,vector <double>);
+vector <vector<double>> g(double,vector <double>,vector <double>);
 
 int main()
 {
-  vector <double> init_q {0};
-  vector <double> init_p {1};
-  rk4_s integrate(init_q,init_p,f);
-  integrate.rk4(0.1,100);
+  vector <double> init_q {0}; //initial values of q
+  vector <double> init_p {1}; //initial values of p
+  rk4_s integrate(init_q,init_p,f); //Initializing RK4_S class
+  integrate.rk4(0.1,100); //solving equation
   return 0;
 }
 
-vector <vector<double>> f(vector <double> q,vector <double> p)
+//Hamilton equations 
+vector <vector<double>> f(double t,vector <double> q,vector <double> p)
 {
   double k = 1;
   double m = 1;
 
-  double xpoint = p[0]/m;
+  double xpoint = p[0]/m; 
   double ppoint = -k*q[0];
   vector <vector<double>> variables {{xpoint},{ppoint}};
   return variables;
-  
 }
+
+
+
+
